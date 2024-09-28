7722507b3293e34f3e1b2c97ef238da55ebe75e8
Valdiney Nardes <valdineynardes@gmail.com>
2024-09-27 23:24:24 -0300

    Resolução da atividade de PWEBII cadastro de produtos.

---
+++ cadastro_de_produto/salvar_produto.php
@@ -0,0 +1,18 @@
+<?php
+$codigo = $_GET['codigo'];
+$nome = $_GET['nome'];
+$descricao = $_GET['descricao'];
+$valor = $_GET['valor'];
+
+
+$delimitador = '%&&&%';
+$dadosProduto = $codigo . $delimitador . $nome . $delimitador . $descricao . $delimitador . $valor . "\n";
+
+$arquivo = fopen("produto.txt", "a");
+fwrite($arquivo, $dadosProduto);
+fclose($arquivo);
+
+
+print '<div class="container mt-3 alert alert-success">Produto salvo com sucesso!</div>';
+?>
+
