0a724bdb7a7611d17d23b01e5f52f873e5d0d5ad
Valdiney Nardes <valdineynardes@gmail.com>
2024-09-29 15:46:26 -0300

    Atualizando o repositório.

--- javascript/atividade_pweb1/app2.js
+++
@@ -1,6 +0,0 @@
-function salarioBase(){
-    let salarioBase = parseFloat(prompt("Digite o seu salário: "));
-    //aumentos e descontos
-    let salarioReceber = salarioBase*1.04
-    document.write("O seu salario é: R$", salarioReceber);
-}
